f574d22738fa519ed236e97a35ea0819ea7b0ec7
Charlie Harrison <charlieisharrison@gmail.com>
2014-07-22 02:40:03 -0400

    generator in C

--- server/speedy/generator.c
+++ server/speedy/generator.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
 #include <sndfile.h>
 #include "kiss_fft.h"
 
 #define CHUNK 1024
 #define BANDS 32
 #define HISTORY_SIZE 10
-#define C 20
+#define C 6
+#define D 7
 /*
       typedef struct
       {    sf_count_t  frames ;
@@ -22,9 +24,9 @@ int samplerate;
 void generate_cpx_from_input(kiss_fft_cpx *in, int size, short *raw_in, int raw_size) {
     // raw_in = {r,i,r,i,r,i,...}
     int i;
-    for(i = 0; i < size; i = i + 1) {
-        in[i].r = raw_in[i];
-        in[i].i = raw_in[i + size];
+    for(i = 0; i < size; i++) {
+        in[i].r = raw_in[i*2];
+        in[i].i = raw_in[i*2 + 1];
     }
 }
 
@@ -35,29 +37,22 @@ int main(int argc, char **argv) {
     }
     char *path = argv[1];
 
-
-
-
     SNDFILE *infile;
     SF_INFO sfinfo;
     infile = sf_open(path, SFM_READ, &sfinfo);
+    /*
     printf("%10s : %lu\n", "frames", sfinfo.frames);
     printf("%10s : %d\n", "samplerate", sfinfo.samplerate);
     printf("%10s : %d\n", "channels", sfinfo.channels);
     printf("%10s : %d\n", "format", sfinfo.format);
     printf("%10s : %d\n", "sections", sfinfo.sections);
     printf("%10s : %d\n", "seekable", sfinfo.seekable);
+    */
     samplerate = sfinfo.samplerate;
-    int history[BANDS][HISTORY_SIZE];
-    int i;
-    for(i = 0; i < BANDS; i++) {
-        int j;
-        for(j = 0; j < HISTORY_SIZE; j++) {
-            history[i][j] = 0;
-        }
-    }
+    kiss_fft_scalar history[BANDS][HISTORY_SIZE];
+    memset(history, 0, sizeof(kiss_fft_scalar) * BANDS * HISTORY_SIZE);
 
-    kiss_fft_cfg cfg = kiss_fft_alloc(CHUNK, 0, NULL, NULL);
+    //kiss_fft_cfg cfg = kiss_fft_alloc(CHUNK, 0, NULL, NULL);
     kiss_fft_cpx in[CHUNK];
     kiss_fft_cpx out[CHUNK];
     kiss_fft_scalar out_real[CHUNK];
@@ -67,56 +62,50 @@ int main(int argc, char **argv) {
     sf_count_t count;
     while((count = sf_readf_short(infile, raw_in, CHUNK)) == CHUNK) {
         generate_cpx_from_input(in, CHUNK, raw_in, CHUNK*2);
-        analyze_chunk(&cfg, in, CHUNK, out, out_real, history);
+        analyze_chunk(in, CHUNK, out, out_real, history);
     }
 
     sf_close(infile);
     return 0;
 }
 
-int
+kiss_fft_scalar
 remember(int band_num,
          kiss_fft_scalar *out_real,
          int out_size,
          kiss_fft_scalar history[BANDS][HISTORY_SIZE])
 {
-    int band_size = out_size / BANDS;
+    int band_size = CHUNK / BANDS;
+    assert(band_size == 32);
+
     int start = band_num * band_size;
     kiss_fft_scalar energy = 0;
     int i;
     for(i = start; i < start+band_size; i++) {
-        energy = energy + out_real[i];
+        energy += (float)out_real[i];
     }
-    // energy = (energy * BANDS) / out_size;
-
 
     kiss_fft_scalar avg = 0;
     for (i = 0; i < HISTORY_SIZE; i++){
         avg = avg + history[band_num][i];
     }
-    avg = avg / (HISTORY_SIZE);
+    avg = avg / (float)(HISTORY_SIZE);
 
-    //printf("energy: %f avg: %f\n", energy, avg);;
-    int BEAT = energy > C * avg ? 1 : 0;
-
-    // now place this energy in our history
     int index = (chunk_num % (HISTORY_SIZE));
-    // (30 % 31) + 1 = 31
-    // (31 % 31) + 1 = 1
-    // (32 % 31) + 1 = 2
     history[band_num][index] = energy;
 
-    return BEAT;
+    return avg ? energy / avg : 0;
 }
 
-int analyze_chunk(kiss_fft_cfg *cfg, // will always be same dimensions
-                  kiss_fft_cpx *in,
+int analyze_chunk(kiss_fft_cpx *in,
                   int in_size,
                   kiss_fft_cpx *out,
                   kiss_fft_scalar *out_real,
                   kiss_fft_scalar history[BANDS][HISTORY_SIZE])
 {
-    kiss_fft(*cfg, in, out);
+    kiss_fft_cfg ccfg = kiss_fft_alloc(CHUNK, 0, NULL, NULL);
+    assert(in_size == CHUNK);
+    kiss_fft(ccfg, in, out);
 
     int i;
     for(i = 0; i < in_size; i++) {
@@ -124,26 +113,32 @@ int analyze_chunk(kiss_fft_cfg *cfg, // will always be same dimensions
         // square of the modulus
         out_real[i] = (c.r * c.r) + (c.i * c.i);
     }
+    printf("\n");
+    kiss_fft_free(ccfg);
 
-    char BEATS[BANDS+1];
-    memset(BEATS, '\0', BANDS+1);
+    kiss_fft_scalar BEATS[BANDS];
+    memset(BEATS, 0, BANDS);
+
+    float t = (float)(CHUNK * chunk_num) / samplerate;
 
     int band_size = in_size / BANDS;
-    for(i = 0; i < BANDS; i++) {
-        int BEAT = remember(i, out_real, in_size, history);
-        BEATS[i] = BEAT ? '|' : ' ';
+
+    int logit = 0;
+    int band;
+    for(band = 0; band < BANDS; band++) {
+        kiss_fft_scalar BEAT = remember(band, out_real, in_size, history);
+
+        BEATS[band] = BEAT;
+        if (BEAT > C) logit = 1;
     }
-
-    int s = 0;
-    for(i = 0; i < BANDS; i++) s += BEATS[i] == '|' ? 1 : 0;
-
-    if (s > 0) {
-        float t = (float)(CHUNK * chunk_num) / samplerate;
-        int s = (int)t / 60;
-        t = t - 60*s;
-        printf("%d:%05.2f : %s",s, t, BEATS);
+    if (logit == 1) {
+        printf("%05.2f:", t);
+        for(i = 0; i < BANDS; i++) {
+            printf("%.2f ", BEATS[i]);
+        }
         printf("\n");
     }
+
     chunk_num++;
 }
 
