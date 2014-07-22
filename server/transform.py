8077f35ea2635a39ee256f787cc9e874f3dc4a6e
Charlie Harrison <charlieisharrison@gmail.com>
2014-07-22 02:41:05 -0400

    cleanup

--- server/transform.py
+++ server/transform.py
@@ -4,14 +4,15 @@ import subprocess
 import os
 from pydub import AudioSegment
 import wave
+import generate
 
-def convert(inp, name, out_dir):
+def convert(inp, name, out_dir, led_dir):
 	song = AudioSegment.from_mp3(inp)
 
 	out = "%s/%s.wav" % (out_dir, name)
 
 	song.export(out, format="wav")
-
+	generate.make_led(out, name, led_dir)
 
 class Transformer(object):
 
@@ -40,7 +41,7 @@ class Transformer(object):
 				working.append(name)
 		return (sorted(s) for s in (done, working, errored))
 
-	def start(self, f, name, out):
+	def start(self, f, name, out, led_out):
 		if name in self.progress:
 			return "already have this song"
 		else:
@@ -48,15 +49,21 @@ class Transformer(object):
 			tmp = "%s/%s" % (config.TMP_DIRECTORY, name)
 			f.save(tmp)
 			name = "".join(name.split(".")[:-1])
-			aresult = self.pool.apply_async(convert, (tmp, name, out))
+			aresult = self.pool.apply_async(convert, (tmp, name, out, led_out))
 			self.progress[name] = aresult
 
 
 transformer = Transformer()
 
 def start(f, name):
-	return transformer.start(f, name, config.MUSIC_DIRECTORY)
+	return transformer.start(f, name, config.MUSIC_DIRECTORY, config.LED_DIRECTORY)
 
 def progress():
 	return transformer.get_progress()
 
+
+if __name__ == "__main__":
+	f = open("/home/charlie/Dropbox/warondrugs/01 Under The Pressure.mp3",'r')
+	out = config.MUSIC_DIRECTORY
+	out_led = config.LED_DIRECTORY
+	convert(f, '01_Under_The_Pressure', out, out_led)
